7bbdf847fbda4372b236806659b8ea95dca5c968
GABRIEL FRANCISCO SANTOS SOUSA <gabriel.fss@puccampinas.edu.br>
2024-05-21 11:18:32 -0300

    install breeze

--- app/Models/Secretaria.php
+++ app/Models/Secretaria.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Secretaria extends Model
 {
     use HasFactory;
+
+    protected $table = "secretarias";
+
+    protected $fillable = [
+        'Nome',
+        'Sobrenome',
+        'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
