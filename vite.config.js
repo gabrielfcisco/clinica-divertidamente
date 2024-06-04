7c2a7b13b7a6d5fc16ae4ca4cf499ca6f4723e87
gabrielfcisco <gabrifran017@gmail.com>
2024-06-04 01:18:36 -0300

    kubernetes

--- vite.config.js
+++ vite.config.js
@@ -30,12 +30,15 @@ export default defineConfig({
     },
 
     server: {
+        hmr: {
+            host: 'localhost',
+        },
         host: '0.0.0.0',
         watch: {
             usePolling: true,
         },
-        cors: {
-            origin: '*',
-        },
+        // cors: {
+        //     origin: '*',
+        // },
     },
 });
